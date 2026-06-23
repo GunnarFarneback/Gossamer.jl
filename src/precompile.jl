4472082baed2c5b8a65df1c0551554d9f94491a9
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2026-06-23 16:33:19 +0200

    Precompile a little more.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,2 +1,5 @@
 precompile(main, (Vector{String},))
 precompile(Tuple{typeof(Base.showerror), Base.TTY, JuliaSyntax.ParseError})
+
+precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:bold, :italic, :underline, :blink, :reverse, :hidden, :color), Tuple{Bool, Bool, Bool, Bool, Bool, Bool, Symbol}}, typeof(Base.printstyled), Base.TTY, String, Vararg{String}})
+precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:bold, :italic, :underline, :blink, :reverse, :hidden), NTuple{6, Bool}}, typeof(Base.with_output_color), Function, Symbol, Base.TTY, String, Vararg{String}})
